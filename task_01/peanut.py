b9decec1d694bf94715187d37344bc4032673bc2
Djamshed Djuraev <djamshed.djuraev@spsmail.cuny.edu>
2016-09-24 05:19:26 +0000

    Task 01 to 03 completed

---
+++ task_01/peanut.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring for True or False"""
+
+BUTTER = True
+OIL = False
---
+++ task_03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring for copying a module attribute."""
+
+from task_01.peanut import BUTTER
+
+JELLY = BUTTER
